a1de6aeddf2ca61fef2f64b52ac344ad004f8e61
RAHUL SAINI <110321253+Rahulsaini27@users.noreply.github.com>
2025-08-09 11:45:20 +0530

    Update authRoutes.js reslove error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerAdmin, loginUser } = require('../controllers/authController');
 
-// router.post('/register-admin', registerAdmin); we are making this route proctive beause it not for public use  
+router.post('/register-admin', registerAdmin);  
 router.post('/login', loginUser);
 
 module.exports = router;
